b7a7e57e9830f47fbee0e9a2c5acee98b65973e4
Samarth <samarth.mahendragowda@gmail.com>
2025-05-03 16:20:25 -0400

    new feature with vocie call

--- main.py
+++ main.py
@@ -836,7 +836,7 @@ async def initialize_session(openai_ws):
                 {
                     "type": "function",
                     "name": "end_call",
-                    "description": "Function to end the call",
+                    "description": "Function to end the call after the conversation is done. example when it reaches voice mail, end call after saying message",
                     "parameters": {
                         "type": "object",
                         "properties": {
